36d96c9edcdb8912c099976bbabf8e63b407e56d
Peng Zhou <peng.zhou@progress.com>
2025-06-02 13:13:22 -0700

    fix the missing RBAC

--- internal/controller/marklogiccluster_controller.go
+++ internal/controller/marklogiccluster_controller.go
@@ -44,6 +44,7 @@ type MarklogicClusterReconciler struct {
 //+kubebuilder:rbac:groups=marklogic.progress.com,resources=marklogicclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=marklogic.progress.com,resources=marklogicclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=marklogic.progress.com,resources=marklogicclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
